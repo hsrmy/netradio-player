d3d1a208bcb7cbc4f935d4c15e54d0ca7dffee02
hsrmy <hsrmy@emradc.tk>
2018-10-04 04:46:09 +0900

    Initial Commit

---
+++ netradio player/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  netradio player
+//
+//  Created by hsrmy on 2018/10/4.
+//  Copyright © 2018 hsrmy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
